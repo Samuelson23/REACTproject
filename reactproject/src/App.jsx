235c95b4a4b8011a62c1750fd5ad7307b3f39e6f
cmdt367 <cmdt367@gmail.com>
2023-06-07 19:37:28 +0200

    git commit

--- reactproject/src/App.jsx
+++ reactproject/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Register from './components/Register/Register'
-
+import Footer from './components/Footer/Footer';
+Footer
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+<Footer/>
 
     </>
   )
--- reactproject/src/components/Register/Register.jsx
+++ reactproject/src/components/Register/Register.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { useState } from 'react';
+import './Register.css';
 
 const Register = () => {
   return (
     <div>
-      
+      <div className="header">
+        <div className="title">Hotel Castelldefels</div>
+        <div className="buttons">
+          <button>Home</button>
+          <button>Login</button>
+        </div>
+      </div>
+      <div className="content">
+        <div className="register-text">Register</div>
+        <div className="form">
+          <input type="text" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input type="text" placeholder="Username" />
+        </div>
+        <img src="ruta_de_la_imagen.jpg" alt="Imagen" />
+        <div className="buttons">
+          <button>Register</button>
+          <button>Login</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
+
