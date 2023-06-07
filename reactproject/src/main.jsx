d99f27343cfbbde20cacb039722bf741a99d4b82
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2023-06-07 17:25:14 +0200

    ain

--- reactproject/src/main.jsx
+++ reactproject/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Profile from './components/Profile/Profile.jsx'
-Dash
+import Dashboard from './components/Dashboard/Dashboard.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
