ba9bdeda4b04651b9cc085a07102640e7811c06a
cmdt367 <cmdt367@gmail.com>
2023-06-07 17:08:11 +0200

    Register

--- reactproject/src/App.jsx
+++ reactproject/src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import Register from './components/Register/Register'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+    <Register/>  
     </>
   )
 }
 
 export default App
+
+
+
