97bf772267ca7c85204f782adad501a84ff70ccf
cmdt367 <cmdt367@gmail.com>
2023-06-07 19:37:05 +0200

    git commit

--- reactproject/src/components/Footer/Footer.jsx
+++ reactproject/src/components/Footer/Footer.jsx
@@ -1,13 +1,38 @@
-import "./Footer.css"
+import React from 'react';
+import './App.css';
+import Register from './components/Register/Register';
+import Footer from './components/Footer/Footer';
 
-import React from 'react'
 
-const Footer = () => {
+
+function App() {
   return (
-    <div>
-      
+    <div className="app">
+      <header className="header">
+        <div className="title">Hotel Castelldefels</div>
+        <div className="buttons">
+          <button>Home</button>
+          <button>Login</button>
+        </div>
+      </header>
+
+      <div className="content">
+        <div className="register-text">Register</div>
+        <div className="form">
+          <input type="text" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input type="text" placeholder="Username" />
+        </div>
+        <img src="ruta_de_la_imagen.jpg" alt="Imagen" />
+        <div className="buttons">
+          <button>Register</button>
+          <button>Login</button>
+        </div>
+      </div>
+
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Footer
+export default App;
