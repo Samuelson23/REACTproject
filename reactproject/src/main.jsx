66d3fd0efb747b47e52d3a04e88ecf286fbd2f86
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2023-06-07 17:35:50 +0200

    routes

--- reactproject/src/main.jsx
+++ reactproject/src/main.jsx
@@ -4,10 +4,28 @@ import App from './App.jsx'
 import './index.css'
 import Profile from './components/Profile/Profile.jsx'
 import Dashboard from './components/Dashboard/Dashboard.jsx'
+import Events from './components/Events/Events.jsx'
+import Login from './components/Login/Login.jsx'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    
+    <BrowserRouter basename='/'>
+
+      <Routes>
+
+        <Route path='/' element={<App />}>
+          <Route index element={<Dashboard />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+
+      </Routes>
+
+    </BrowserRouter>
+
   </React.StrictMode>,
 )
