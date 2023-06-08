8eb77a97e478d49276889b06b7569f355a9265f1
Samuelson23 <3samuelmg@gmail.com>
2023-06-08 13:02:25 +0200

    register

--- reactproject/src/components/Register/Register.jsx
+++ reactproject/src/components/Register/Register.jsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import "./Register.css";
 import React, { useEffect, useState } from "react";
 import { registerUser } from "../../services/user.service";
+import Uploadfile from "../Uploadfile";
 
 //1º) Crear el html de lo que será la estructura del register para darle un poco de forma
 //2º) Importar la libreria Hook-form para poder gestionar bien los formularios
@@ -9,12 +10,28 @@ import { registerUser } from "../../services/user.service";
 
 const Register = () => {
   const { register, handleSubmit } = useForm(); //handleSubmit nos permite controlar el formulario mediante funciones, creo
-  const [resp, setResp] = useState({})
-  const [send, setSend] = useState()
+  const [resp, setResp] = useState({}) //Estado en el que seteamos la respuesta y hacemos la llamada al servicio con la informacion recogida en los inputs
+  const [send, setSend] = useState() //Estado para deshabilitar el boton de registrar hacemos la llamada al servicio
 
+
+  //Creamos la funcion formSubmit que recopila la info de los inputs y le añade la imagen. Junto a handleSubmit nos permite recopilar toda la 
+  //informacion que recogemos por los inputs (formData)
   const formSubmit = async (formData) => {
-    console.log(formData)
-    setResp(await registerUser(formData))
+    const inputFile = document.getElementById("file-upload").files;
+    console.log(inputFile)
+    let customData;
+
+    if(inputFile.length!==0){
+      customData = {...formData, imagen:inputFile[0]}
+      setSend(true)
+      setResp(await registerUser(customData))
+      setSend(false)
+    }else{
+      setSend(true)
+      setResp(await registerUser(formData))
+      setSend(false)
+    }
+
   }
 
 
@@ -22,12 +39,12 @@ const Register = () => {
     console.log(resp)
   },[resp])
 
-
+  //form onSubmit={handleSubmit(formSubmit)}  -> handleSubmit + formSubmit recopilan toda la info recogida en los inputs
   return (
     <div className="divFormulario">
       <h2>Sign UP!</h2>
       <p>Sign for free if you want to see all content</p>
-      <form onSubmit={handleSubmit(formSubmit)}>
+      <form onSubmit={handleSubmit(formSubmit)}> 
         <div className="inputContainer">
           <label htmlFor="custom-input">Username:</label>
           <input
@@ -80,13 +97,7 @@ const Register = () => {
                 {...register("gender")}
               />
             </div>
-            <input
-                type="file"
-                name="image"
-                id="image"
-               
-                {...register("imagen")}
-              />
+            <Uploadfile/>
         </div>
         
         <button type="submit" disabled={send} style={{background: "#49c1a388"}}>REGISTRAR</button>
--- reactproject/src/utils/updateToken.js
+++ reactproject/src/utils/updateToken.js
@@ -2,7 +2,7 @@ export const updateToken = () => {
   const user = localStorage.getItem("user");
 
   if (user) {
-    const parseUser = JSON.parse(urse);
+    const parseUser = JSON.parse(user);
     return parseUser.token;
   }
 };
