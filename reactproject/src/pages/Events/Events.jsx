7138bdf8b8d3205a622cdecc961449060280a03e
ozillo <marcmateo25@gmail.com>
2023-06-08 16:05:04 +0200

    Events

--- reactproject/src/pages/Events/Events.jsx
+++ reactproject/src/pages/Events/Events.jsx
@@ -2,10 +2,24 @@ import React from 'react'
 
 const Events = () => {
   return (
-    <div>
-      
+    <div className="event-page">
+      <h1>Eventos</h1>
+      <div className="event-list">
+        <div className="event">
+          <h2>Evento 1</h2>
+          <p>Descripción del evento 1.</p>
+        </div>
+        <div className="event">
+          <h2>Evento 2</h2>
+          <p>Descripción del evento 2.</p>
+        </div>
+        <div className="event">
+          <h2>Evento 3</h2>
+          <p>Descripción del evento 3.</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Events
