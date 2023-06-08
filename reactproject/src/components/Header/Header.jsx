b45f28b3d641d5cb0d63cd70f39d7061b9943000
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2023-06-08 15:33:54 +0200

    nav-photos

--- reactproject/src/components/Header/Header.jsx
+++ reactproject/src/components/Header/Header.jsx
@@ -5,56 +5,64 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      <header>
-        <div>
+
+    <header>
+
+      <div>
+
         <img
             src="https://res.cloudinary.com/dw9b8eqmc/image/upload/v1686179496/proyectoREACT/hotellogo_qcrsyu.png"
             alt="logo"
             className="Logo"
-          />
-        </div>
-        <nav className="NavIcons">
-  <NavLink to="/dashboard">
-    <button> 
-    <img
-      src="https://res.cloudinary.com/dw9b8eqmc/image/upload/v1686179869/proyectoREACT/smart-home_vsyo0i.png"
-      alt="Home"
-    />
-    </button>
+        />
+
+        <h1 className="hotel-title">HOTEL CASTEFA</h1>
+
+      </div>
+
+      <nav className="NavIcons">
+
+    <NavLink to="/dashboard">
+      <button> 
+        <img
+          className="nav-img"
+          src="https://res.cloudinary.com/dw9b8eqmc/image/upload/v1686179869/proyectoREACT/smart-home_vsyo0i.png"
+          alt="Home"
+        />
+      </button>
     
-  </NavLink>
-  <NavLink to="/Events">
-    <button>
-      <img
-        src="https://res.cloudinary.com/dw9b8eqmc/image/upload/v1686180327/proyectoREACT/calendar-check_rgbc42.png"
-        alt="Events"
-      />
-      Events
-    </button>
-  </NavLink>
-  <NavLink to="/Profile">
-    <button>
-      <img
-        src="https://res.cloudinary.com/dw9b8eqmc/image/upload/v1686180319/proyectoREACT/user_hnxorp.png"
-        alt="Profile"
-      />
-      Perfil
-    </button>
-  </NavLink>
-  <NavLink to="/Logout">
-    <button>
-      <img
-        src="https://res.cloudinary.com/dw9b8eqmc/image/upload/v1686180517/proyectoREACT/logout_mxofcx.png"
-        alt="Logout"
-      />
-      Logout
-    </button>
-  </NavLink>
-</nav>
-    </header>
+    </NavLink>
+
+    <NavLink to="/Events">
+      <button>
+        <img
+          src="https://res.cloudinary.com/dw9b8eqmc/image/upload/v1686180327/proyectoREACT/calendar-check_rgbc42.png"
+          alt="Events"
+        />
+      </button>
+    </NavLink>
+
+    <NavLink to="/Profile">
+      <button>
+        <img
+          src="https://res.cloudinary.com/dw9b8eqmc/image/upload/v1686180319/proyectoREACT/user_hnxorp.png"
+          alt="Profile"
+        />
+      </button>
+    </NavLink>  
+
+    <NavLink to="/Logout">
+      <button>
+        <img
+          src="https://res.cloudinary.com/dw9b8eqmc/image/upload/v1686180517/proyectoREACT/logout_mxofcx.png"
+          alt="Logout"
+        />
+      </button>
+    </NavLink>
+
+  </nav>
+  </header>
       
-    </div>
   )
 }
 
