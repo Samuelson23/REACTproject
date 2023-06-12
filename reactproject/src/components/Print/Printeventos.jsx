30dac3a86bc10cb4c4540d99e7063a3a077d63a6
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2023-06-12 11:18:48 +0200

    events

--- reactproject/src/components/Print/Printeventos.jsx
+++ reactproject/src/components/Print/Printeventos.jsx
@@ -2,25 +2,19 @@ import "./Printeventos.css"
 
 export const PrintEvent = ({data}) => {
     console.log("datacomp",data)
-    const {name, description, hora, location,_id} = data
+    const {name, description, hora, location, _id} = data
     console.log("name",name)
     return (
         <div>
         {data.data.map((element)=>(
-        <div key={element._id} className="event-container">
+        <figure key={element._id} className="event-container">
         <h2>{element.name}</h2>
         <p>{element.description}</p>
         <p>{element.hora}</p>
         <p>{element.location}</p>
-<button>Apuntarse</button>
-
-        </div>
-
-
+        <button>Apuntarse</button>
+        </figure>
         ))
-        
-        
-        
         }
         
         </div>
